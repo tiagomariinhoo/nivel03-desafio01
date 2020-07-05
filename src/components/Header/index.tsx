2e9bfe8d0444091adec347318f0ef6e036c99ff5
Tiago Marinho <tiago.marinho.797@gmail.com>
2020-07-05 14:29:15 -0300

    Finish

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-import { Link } from 'react-router-dom';
-
 import { Container } from './styles';
+
+import { Link } from 'react-router-dom'
 
 import Logo from '../../assets/logo.svg';
 
@@ -16,7 +16,10 @@ const Header: React.FC<HeaderProps> = ({ size = 'large' }: HeaderProps) => (
       <img src={Logo} alt="GoFinances" />
       <nav>
         {
-          // Todo
+          <>
+            <Link to="/">Listagem</Link>
+            <Link to="/import">Importar</Link>
+          </>
         }
       </nav>
     </header>
